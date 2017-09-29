10fff082647d2df9f73518654592ee698b012f40
rcowles3 <ryan.cowles3@gmail.com>
2017-09-29 12:10:08 -0700

    fixed connection var in server.js. let to const.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 class helpers {
     sendData(data) {
+        // let localCon = 'http://localhost:4200/users/create';
+        // let herokuCon = 
         console.log("DATA", data);
-        axios.post('http://localhost:4200/users/create', {
+        axios.post('/users/create', {
             CreateUser: data
         })
             .then(res => this.setState({ createUser: res.data })
@@ -13,7 +15,7 @@ class helpers {
 
     authData(loginInfo) {
         console.log("Login Auth: ", loginInfo);
-        axios.post('http://localhost:4200/users/authenticate', {
+        axios.post('/users/authenticate', {
             loginInfo: loginInfo
         })
             .then(res => this.setState({ loginInfo: res.data }))
