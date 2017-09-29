7dc801a96ed0645f135227fff7704710770dc97f
rcowles3 <ryan.cowles3@gmail.com>
2017-09-29 11:12:10 -0700

    updated userroutes to res.json

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -44,7 +44,7 @@ userRouter.route("/authenticate").post((req, res) => {
     auth = (err, info, doc) => {
         if (err) throw err;
         console.log("\n\n\nReturned data from DB", info);  
-        res.json(info);
+        res.json(info); // send auth user to be grabbed by json
     }
 
     CreateUser.
